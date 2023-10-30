94c0a31841ef806bea02b72b92c97fffa7735a1a
leomckenna <leomckenna2025@u.northwestern.edu>
2023-10-29 22:22:23 -0500

    finished work

--- McKenna_Leo_final-project.R
+++ McKenna_Leo_final-project.R
@@ -4,23 +4,33 @@
 ## load packages and data----
 library(tidyverse)
 #read in data
-elo_data <- read_csv("data/nfl_elo.csv")
+nfl_elo_data <- read_csv("data/nfl_elo.csv")
+
+## Data quality & complexity check
+#There are 33 total variables and 17,379 observations. 
+nfl_elo_data |> 
+  select(where(is.numeric))
+
+nfl_elo_data |> 
+  filter(!is.na(total_rating))
+  
+
 
 #playing around with data
-elo_data |> 
+nfl_elo_data |> 
   filter(!is.na(playoff)) |> 
   summarize(
     mean_game_elo_pre = mean((elo1_pre + elo2_pre)/2)
   ) |> 
   arrange(desc(mean_game_elo_pre))
 
-elo_data |> 
+nfl_elo_data |> 
   filter(is.na(playoff)) |> 
   summarize(
     mean_game_elo_pre = mean((elo1_pre + elo2_pre)/2)
   ) |> 
   arrange(desc(mean_game_elo_pre))
 
-elo_data |> 
+nfl_elo_data |> 
   filter(elo1_pre > 1800 | elo2_pre > 1800,
          is.na(playoff))
