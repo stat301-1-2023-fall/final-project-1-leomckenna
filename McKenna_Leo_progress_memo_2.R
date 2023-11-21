75174e44b256a2cae7f481598104616882d771ca
leomckenna <leomckenna2025@u.northwestern.edu>
2023-11-20 18:45:03 -0600

    work with weather data

--- McKenna_Leo_progress_memo_2.R
+++ McKenna_Leo_progress_memo_2.R
@@ -6,22 +6,37 @@ library(tidyverse)
 library(skimr)
 #read in data
 nfl_elo_data <- read_csv("data/nfl_elo.csv")
-draft_picks <- 
-  read_csv("https://raw.githubusercontent.com/leesharpe/nfldata/master/data/draft_picks.csv")
+foxboro_weather <- read_csv("data/foxboro_weather_data.csv")
+
+#cleaning data
+foxboro_weather_cleaned <- foxboro_weather |> 
+  select(-c(1, 16)) |> 
+  slice(-c(1, 23, 24, 25, 26, 32)) |> 
+  slice(-27)
+
+foxboro_weather_cleaned %>%
+  setNames(foxboro_weather_cleaned[1, ]) %>%
+  slice(-1) %>%
+  pivot_longer(cols = -c(Year, Annual), names_to = "Month", values_to = "Value") %>%
+  mutate(across(-c(Year, Annual, Month), ~ifelse(. == "M", NA, as.numeric(.)))) %>%
+  group_by(Year) %>%
+  summarize(
+    Mean = mean(Value, na.rm = TRUE),
+    Max = max(Value, na.rm = TRUE),
+    Min = min(Value, na.rm = TRUE)
+  ) %>%
+  bind_cols(select(foxboro_weather_cleaned, Year, Annual)) %>%
+  arrange(Year)
+
+  
 
 ##joining data
 #searching for intersecting names
 names(nfl_elo_data) |> 
   intersect(names(draft_picks))
 
-draft_picks <- draft_picks |> 
-  mutate(team_drafted = team)
-
 nfl_elo_data |> 
-  filter(season >= 1980, team1 == "NE" | team2 == "NE") |> 
+  filter(season >= 2000, team1 == "NE" | team2 == "NE")
 
 
-draft_picks |> 
-  filter(team == "NE",
-         round == 1)
 
