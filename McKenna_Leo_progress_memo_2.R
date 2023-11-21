1c10d92d189412add171ed6f161822303b549ed4
leomckenna <leomckenna2025@u.northwestern.edu>
2023-11-21 00:51:17 -0600

    work in rscript

--- McKenna_Leo_progress_memo_2.R
+++ McKenna_Leo_progress_memo_2.R
@@ -21,25 +21,54 @@ foxboro_weather_cleaned <- foxboro_weather_cleaned |>
 foxboro_weather_cleaned$Value[foxboro_weather_cleaned$Value == "M"] <- NA
 foxboro_weather_cleaned$Value <- as.numeric(foxboro_weather_cleaned$Value)
   
-foxboro_weather_cleaned |>   
+foxboro_weather_cleaned <- foxboro_weather_cleaned |>   
   group_by(Year) |> 
   summarize(
     Mean = mean(Value, na.rm = TRUE),
     Max = max(Value, na.rm = TRUE),
     Min = min(Value, na.rm = TRUE)
-  ) |> 
-  bind_cols(select(foxboro_weather_cleaned, Year, Annual)) %>%
-  arrange(Year)
+  ) 
 
+foxboro_weather_cleaned
   
 
 ##joining data
 #searching for intersecting names
 names(nfl_elo_data) |> 
-  intersect(names(draft_picks))
+  intersect(names(foxboro_weather_cleaned))
 
-nfl_elo_data |> 
-  filter(season >= 2000, team1 == "NE" | team2 == "NE")
+pats_elo_data <- nfl_elo_data |> 
+  filter(season >= 2000, team1 == "NE" | team2 == "NE") |> 
+  group_by(season) 
+
+
+pats_elo_results <- data.frame()
+
+for (i in 1:nrow(pats_elo_data)) {
+  team1_is_ne <- pats_elo_data[i, "team1"] == "NE"
+  team2_is_ne <- pats_elo_data[i, "team2"] == "NE"
+  if (team1_is_ne) {
+    selected_columns <- c("date", "season", "neutral", "playoff", "team1", 
+                          "elo1_pre", "elo2_pre", "elo_prob1", "elo_prob2", "elo1_post",
+                          "elo2_post" , "qbelo1_pre" , "qbelo2_pre" , "qb1", "qb2",
+                          "qb1_value_pre" , "qb2_value_pre" , "qb1_adj" ,"qb2_adj" , "qbelo_prob1" ,
+                           "qbelo_prob2" , "qb1_game_value" ,"qb2_game_value" , "qb1_value_post" ,
+                          "qb2_value_post" , "qbelo1_post" , "qbelo2_post" )
+  } else if (team2_is_ne) {
+    selected_columns <- c("date", "season", "neutral", "playoff", "team1", 
+                          "elo1_pre", "elo2_pre", "elo_prob1", "elo_prob2", "elo1_post",
+                          "elo2_post" , "qbelo1_pre" , "qbelo2_pre" , "qb1", "qb2",
+                          "qb1_value_pre" , "qb2_value_pre" , "qb1_adj" ,"qb2_adj" , "qbelo_prob1" ,
+                          "qbelo_prob2" , "qb1_game_value" ,"qb2_game_value" , "qb1_value_post" ,
+                          "qb2_value_post" , "qbelo1_post" , "qbelo2_post" )
+  } else {
+    # If NE is not in team1 or team2, skip the iteration
+    next
+  }
+  row_data <- pats_elo_data[i, selected_columns, drop = FALSE]
+}
+
+
 
 
 
