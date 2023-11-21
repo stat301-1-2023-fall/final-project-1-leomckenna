c2f7c2e224349e96076261ef467eeed33a2229bc
leomckenna <leomckenna2025@u.northwestern.edu>
2023-11-20 18:02:02 -0600

    looking for secondary data sets

--- McKenna_Leo_progress_memo_2.R
+++ McKenna_Leo_progress_memo_2.R
@@ -6,6 +6,22 @@ library(tidyverse)
 library(skimr)
 #read in data
 nfl_elo_data <- read_csv("data/nfl_elo.csv")
+draft_picks <- 
+  read_csv("https://raw.githubusercontent.com/leesharpe/nfldata/master/data/draft_picks.csv")
+
+##joining data
+#searching for intersecting names
+names(nfl_elo_data) |> 
+  intersect(names(draft_picks))
+
+draft_picks <- draft_picks |> 
+  mutate(team_drafted = team)
 
 nfl_elo_data |> 
-  mutate()
+  filter(season >= 1980, team1 == "NE" | team2 == "NE") |> 
+
+
+draft_picks |> 
+  filter(team == "NE",
+         round == 1)
+
