33bb76cef0efb3b8231a14cc1311cf5c74ef28b5
leomckenna <leomckenna2025@u.northwestern.edu>
2023-11-21 01:21:08 -0600

    progress on weather and pats data sets

--- McKenna_Leo_progress_memo_2.R
+++ McKenna_Leo_progress_memo_2.R
@@ -24,51 +24,49 @@ foxboro_weather_cleaned$Value <- as.numeric(foxboro_weather_cleaned$Value)
 foxboro_weather_cleaned <- foxboro_weather_cleaned |>   
   group_by(Year) |> 
   summarize(
-    Mean = mean(Value, na.rm = TRUE),
-    Max = max(Value, na.rm = TRUE),
-    Min = min(Value, na.rm = TRUE)
+    mean_temp = mean(Value, na.rm = TRUE),
+    max_temp = max(Value, na.rm = TRUE),
+    min_temp = min(Value, na.rm = TRUE)
   ) 
 
-foxboro_weather_cleaned
   
-
-##joining data
 #searching for intersecting names
 names(nfl_elo_data) |> 
   intersect(names(foxboro_weather_cleaned))
 
+#filtering data and getting rid of unnecessary teams
 pats_elo_data <- nfl_elo_data |> 
   filter(season >= 2000, team1 == "NE" | team2 == "NE") |> 
   group_by(season) 
 
+pats_elo_data <- pats_elo_data |> 
+  filter(team1 == "NE" | team2 == "NE") |> 
+  mutate(
+    team = if_else(team1 == "NE", team1, team2),
+    elo_pre = if_else(team1 == "NE", elo1_pre, elo2_pre),
+    elo_prob = if_else(team1 == "NE", elo_prob1, elo_prob2),
+    elo_post = if_else(team1 == "NE", elo1_post, elo2_post),
+    qbelo_pre = if_else(team1 == "NE", qbelo1_pre, qbelo2_pre),
+    qb = if_else(team1 == "NE", qb1, qb2),
+    qb_value_pre = if_else(team1 == "NE", qb1_value_pre, qb2_value_pre),
+    qb_adj = if_else(team1 == "NE", qb1_adj, qb2_adj),
+    qbelo_prob = if_else(team1 == "NE", qbelo_prob1, qbelo_prob2),
+    qb_game_value = if_else(team1 == "NE", qb1_game_value, qb2_game_value),
+    qb_value_post = if_else(team1 == "NE", qb1_value_post, qb2_value_post),
+    qbelo_post = if_else(team1 == "NE", qbelo1_post, qbelo2_post)
+  ) |> 
+  select(date, season, neutral, playoff, team, elo_pre, elo_prob, elo_post,
+         qbelo_pre, qb, qb_value_pre, qb_adj, qbelo_prob, qb_game_value,
+         qb_value_post, qbelo_post, quality, importance, total_rating)
 
-pats_elo_results <- data.frame()
+#joining data
 
-for (i in 1:nrow(pats_elo_data)) {
-  team1_is_ne <- pats_elo_data[i, "team1"] == "NE"
-  team2_is_ne <- pats_elo_data[i, "team2"] == "NE"
-  if (team1_is_ne) {
-    selected_columns <- c("date", "season", "neutral", "playoff", "team1", 
-                          "elo1_pre", "elo2_pre", "elo_prob1", "elo_prob2", "elo1_post",
-                          "elo2_post" , "qbelo1_pre" , "qbelo2_pre" , "qb1", "qb2",
-                          "qb1_value_pre" , "qb2_value_pre" , "qb1_adj" ,"qb2_adj" , "qbelo_prob1" ,
-                           "qbelo_prob2" , "qb1_game_value" ,"qb2_game_value" , "qb1_value_post" ,
-                          "qb2_value_post" , "qbelo1_post" , "qbelo2_post" )
-  } else if (team2_is_ne) {
-    selected_columns <- c("date", "season", "neutral", "playoff", "team1", 
-                          "elo1_pre", "elo2_pre", "elo_prob1", "elo_prob2", "elo1_post",
-                          "elo2_post" , "qbelo1_pre" , "qbelo2_pre" , "qb1", "qb2",
-                          "qb1_value_pre" , "qb2_value_pre" , "qb1_adj" ,"qb2_adj" , "qbelo_prob1" ,
-                          "qbelo_prob2" , "qb1_game_value" ,"qb2_game_value" , "qb1_value_post" ,
-                          "qb2_value_post" , "qbelo1_post" , "qbelo2_post" )
-  } else {
-    # If NE is not in team1 or team2, skip the iteration
-    next
-  }
-  row_data <- pats_elo_data[i, selected_columns, drop = FALSE]
-}
+pats_elo_data <- pats_elo_data |> 
+  mutate(season = as.character(season)) |> 
+  select(-5)
 
-
-
+pats_elo_data |> 
+  left_join(foxboro_weather_cleaned, by = join_by(season == Year)) |> 
+  select(-c(contains('qb')))
 
 
