9af3e0167407b94cfa8a03874861b5be58baa33d
leomckenna <leomckenna2025@u.northwestern.edu>
2023-12-05 23:20:08 -0600

    added memos folder

---
+++ Memos/McKenna_Leo_progress_memo_1.R
@@ -0,0 +1,36 @@
+# Final Project Progress Memo 1 ----
+# Stat 301-1
+
+## load packages and data----
+library(tidyverse)
+#read in data
+nfl_elo_data <- read_csv("data/nfl_elo.csv")
+
+## Data quality & complexity check
+#There are 33 total variables and 17,379 observations. 
+nfl_elo_data |> 
+  select(where(is.numeric))
+
+nfl_elo_data |> 
+  filter(!is.na(total_rating))
+  
+
+
+#playing around with data
+nfl_elo_data |> 
+  filter(!is.na(playoff)) |> 
+  summarize(
+    mean_game_elo_pre = mean((elo1_pre + elo2_pre)/2)
+  ) |> 
+  arrange(desc(mean_game_elo_pre))
+
+nfl_elo_data |> 
+  filter(is.na(playoff)) |> 
+  summarize(
+    mean_game_elo_pre = mean((elo1_pre + elo2_pre)/2)
+  ) |> 
+  arrange(desc(mean_game_elo_pre))
+
+nfl_elo_data |> 
+  filter(elo1_pre > 1800 | elo2_pre > 1800,
+         is.na(playoff))
